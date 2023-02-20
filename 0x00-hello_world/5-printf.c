1112904e27d8db30dfbe5e98a4a564d5797ca940
seekersalama <omar.bsa@outlook.com>
2023-02-20 09:51:06 +0000

    another testing okay

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point if okay
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success) for all return
  */
 int main(void)
 {
