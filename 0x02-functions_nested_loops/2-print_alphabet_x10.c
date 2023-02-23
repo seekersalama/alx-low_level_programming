c7fd3b729caa62e8c9f94a7f30078e8d2514b29a
seekersalama <omar.bsa@outlook.com>
2023-02-23 23:40:33 +0000

    Multiple main testeeror

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	int n;
 	char *cin = "abcdefghijklmnopqrstuvwxyz";
 
-	for (n = 0; n <= 10; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (i = 0; cin[i] != '\0'; i++)
 		{
