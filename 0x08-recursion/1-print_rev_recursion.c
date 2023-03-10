18cc1c96f529ab100e884128450bfcf940791771
seekersalama <omar.bsa@outlook.com>
2023-03-10 00:45:37 +0000

    0x08 recusrsion into 01

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,10 +10,7 @@ void _print_rev_recursion(char *str)
 	if (!(*str))
 	{
 		return;
-	} else
-	{
-		_print_rev_recursion(str + 1);
-		_putchar(*str);
 	}
+	_print_rev_recursion(str + 1);
+	_putchar(*str);
 }
-
