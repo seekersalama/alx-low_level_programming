bea334a92961bea818cd8af8de05cc6b57b13819
seekersalama <omar.bsa@outlook.com>
2023-03-10 00:58:14 +0000

    0x08 recusrsion into 02

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - check the code
+ *@n: int
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
--- 0x08-recursion/3-main.c
+++ 0x08-recursion/3-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int r;
+
 	r = factorial(1);
 	printf("%d\n", r);
 	r = factorial(5);
@@ -16,4 +17,5 @@ int main(void)
 	printf("%d\n", r);
 	r = factorial(-1024);
 	printf("%d\n", r);
+	return (0);
 }
