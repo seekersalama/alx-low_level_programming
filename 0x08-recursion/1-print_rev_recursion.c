678eb609fa0c6636e1060de26f9393c3e3f308fb
seekersalama <omar.bsa@outlook.com>
2023-03-10 00:40:50 +0000

    0x08 recusrsion into 00

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - check the code
+ *@str: char *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *str)
+
+{
+	if (!(*str))
+	{
+		return;
+	} else
+	{
+		_print_rev_recursion(str + 1);
+		_putchar(*str);
+	}
+}
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+0x08. C - Recursion 
+ Getting deeper into it as it should :) 
