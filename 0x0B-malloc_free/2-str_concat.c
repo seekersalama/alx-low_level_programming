cf4062040133def98aa26708d58c0a96747a27b4
seekersalama <omar.bsa@outlook.com>
2023-03-14 01:09:13 +0000

    0x0B. C - malloc, free and allocate and free 06

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,17 +11,39 @@ char *str_concat(char *s1, char *s2)
 	char *pt;
 	unsigned int i, size, j;
 
-	size = sizeof(s1) + sizeof(s2) - 1;
-	if (size == 0)
-		pt = NULL;
-	else
-		pt = malloc(sizeof(char) * size);
-	if (pt != NULL)
+	if (s1 != NULL && s2 != NULL)
 	{
-		for (i = 0; s1[i] != '\0'; i += 1)
-			*(pt + i) = *(s1 + i);
-		for (j = 0; s2[j] != '\0'; j += 1)
-			*(pt + i + j) = *(s2 + j);
+		size = sizeof(s1) + sizeof(s2) - 1;
+		pt = malloc(sizeof(char) * size);
+		if (pt != NULL)
+		{
+			for (i = 0; s1[i] != '\0'; i += 1)
+				*(pt + i) = *(s1 + i);
+			for (j = 0; s2[j] != '\0'; j += 1)
+				*(pt + i + j) = *(s2 + j);
+			*(pt + i + j) = '\0';
+		}
+	}
+	else if (s1 != NULL)
+	{
+		size = sizeof(s1);
+		pt = malloc(sizeof(char) * size);
+		if (pt != NULL)
+		{
+			for (i = 0; s1[i] != '\0'; i += 1)
+				*(pt + i) = *(s1 + i);
+		}
+		*(pt + i) = '\0';
+	} else
+	{
+		size = sizeof(s2);
+		pt = malloc(sizeof(char) * size);
+		if (pt != NULL)
+		{
+			for (j = 0; s2[j] != '\0'; j += 1)
+				*(pt + j) = *(s2 + j);
+		}
+		*(pt + j) = '\0';
 	}
 	return (pt);
 }
