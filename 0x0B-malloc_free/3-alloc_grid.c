1a583c65043fd18e1e1ff8e3869c0779bd06a386
seekersalama <omar.bsa@outlook.com>
2023-03-14 00:16:02 +0000

    0x0B. C - malloc, free and allocate and free 02

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,26 +11,42 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
+	int sig;
 	int **ptt;
 
+	sig = 0;
 	if (width <= 0 || height <= 0)
 	{
 		ptt = NULL;
+		sig++;
 	}
 	else
+	{
 		ptt = malloc(width * sizeof(*ptt));
+		if(!ptt)
+			sig++;
+	}
 	if (ptt != NULL)
 	{
 		for (i = 0; i < width; i += 1)
-			ptt[i] = malloc(height * sizeof(*ptt[i]));
-	}
-
-	for (i = 0; i < width; i += 1)
-	{
-		for (j = 0; j < height; j += 1)
 		{
-			ptt[i][j] = 0;
+			ptt[i] = malloc(height * sizeof(*ptt[i]));
+			if(!ptt[i])
+				sig++;
 		}
+		for (i = 0; i < width; i += 1)
+		{
+			for (j = 0; j < height; j += 1)
+			{
+				if(&ptt[i][j] != NULL)
+					ptt[i][j] = 0;
+			}
+		}
+	}
+	if(sig)
+	{
+		free(ptt);
+		ptt = NULL;
 	}
 	return (ptt);
 }
