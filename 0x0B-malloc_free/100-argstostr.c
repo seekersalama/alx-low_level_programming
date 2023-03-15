0c623ea20cbf24af6060c3467f431604d43a57f0
seekersalama <omar.bsa@outlook.com>
2023-03-15 00:19:54 +0000

    0x0B. C - malloc, free and allocate and free 22

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,7 +15,7 @@ char *argstostr(int ac, char **av)
 	step = 0;
 	if (ac != 0 && av != NULL)
 	{
-		pt = (char *) malloc(sizeof(char *) * (ac) - 6);
+		pt = (char *) malloc(sizeof(char *) * (ac - 1) - 10);
 		if (pt != NULL)
 		{
 			for (i = 0; av[i] != NULL; i++)
