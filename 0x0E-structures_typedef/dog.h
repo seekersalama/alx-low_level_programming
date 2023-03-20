21dcb6655e5882c59e0a8f69979913b14a5a2624
seekersalama <omar.bsa@outlook.com>
2023-03-20 11:40:18 +0000

    0x0E. C - Structures, typedef 00

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - struc about DOG
+* @name: char * member
+* @age: float member
+* @owner: char * member
+*
+* Description: Longer description
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
