f14619193dbdbcb12f2b3c45d1217ef908cf0533
seekersalama <omar.bsa@outlook.com>
2023-03-20 11:59:42 +0000

    0x0E. C - Structures, typedef 01

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - initialize the doc struct
+ *@d: struct dog *
+ *@name: char *
+ *@age: float
+ *@owner: char *
+ * Return: Always 0.
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+#endif /* MAIN_H */
