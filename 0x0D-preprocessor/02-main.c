3f9cc2a9eee4c6c338d0123b1198099afab2e6c1
seekersalama <omar.bsa@outlook.com>
2023-03-20 20:38:29 +0000

    0x09. C - Static libraries 07

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include "1-pi.h"
+#include <stdio.h>
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+
+char *pt = __BASE_FILE__;
+
+	printf("%s\n", pt);
+	return (0);
+}
