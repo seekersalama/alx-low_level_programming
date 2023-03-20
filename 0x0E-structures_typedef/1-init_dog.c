11f964a66d23df4afc8c796c4e0ad09196cb79ff
seekersalama <omar.bsa@outlook.com>
2023-03-20 12:17:05 +0000

    0x0E. C - Structures, typedef 04

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,11 +5,11 @@
  *@name: char *
  *@age: float
  *@owner: char *
- * Return: Always 0.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
